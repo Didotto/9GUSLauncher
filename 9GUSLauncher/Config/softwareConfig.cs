85f23d25ff811e5c7e04909838516a8283bd9a08
Didotto <basket-one@libero.it>
2015-05-14 14:41:24 +0200

    Beta 1.0.0.1

--- 9GUSLauncher/Config/softwareConfig.cs
+++ 9GUSLauncher/Config/softwareConfig.cs
@@ -3,38 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+ 
 namespace _9GUSLauncher.Config
 {
     public class softwareConfig
     {
         //Online FTP Json config
-
+ 
         public string ftpjsonUser = "";
         public string ftpjsonPassword = "";
         public string ftpjsonAddress = ""; //ftp.domain.com or 127.0.0.1
         public string ftpjsonFolder = ""; // /public_html/private/files/
         public string ftpjsonFileName = "";
-
+ 
         //Online URLS
-
+ 
         public string jsonConfig = ""; //https or http
         public string updateFile = ""; //https or http
         public string eventsLocation = "";
-
+ 
         //Master Server
-
+ 
         public string masterServer = ""; //master.domain.com or 127.0.0.1
-
+ 
         //String Cipher Key
-
+ 
         public readonly string cipherKey = "";
-
+ 
         //Login System Platform
-
+ 
         public string forumLink = ""; //https or http. actually supported phpbb3 (/ucp.php?mode=login)
-
+ 
     }
-
+ 
+ 
 
 }
